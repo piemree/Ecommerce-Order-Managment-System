683ae3117f2ea0d5635be513517fc6b09f9c1677
emre <pi.emree@gmail.com>
2024-05-25 20:09:36 +0300

    Add calculation util

---
+++ utils/decimal.util.js
@@ -0,0 +1,13 @@
+// create a function that takes a number and returns a number with 2 decimal places
+
+const AppError = require("../errors/app.error")
+
+function calc(number) {
+    // check is number is a number
+    if (isNaN(number)) throw AppError.InternalServerError()
+    return parseFloat(number.toFixed(2))
+}
+
+module.exports = {
+    calc
+}
