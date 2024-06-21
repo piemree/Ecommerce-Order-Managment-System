3c56dcff30e7e9b757699582464726b03780c3b8
emre <pi.emree@gmail.com>
2024-06-21 16:16:04 +0300

    Fix category schema

--- schemas/category.schema.js
+++ schemas/category.schema.js
@@ -1,7 +1,7 @@
 const joi = require("joi")
 
 const createCategorySchema = {
-    title: joi.string().email().required(),
+    title: joi.string().required(),
     description: joi.string().required()
 }
 
